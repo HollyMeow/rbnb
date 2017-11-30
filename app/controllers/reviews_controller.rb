9d0a59de0abfea6a8cc3fb53897193c125bbdce8
YM <youssef.meskini@iscparis.com>
2017-11-30 17:04:15 +0100

    Création du controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,19 @@
+class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.all
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    Review.create(review_params)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
+  end
+end
